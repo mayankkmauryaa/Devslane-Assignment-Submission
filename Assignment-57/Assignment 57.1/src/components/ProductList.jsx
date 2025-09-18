29d2c8fb7c45b09cb1f42fcd8a3f53585cdd688b
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-09-18 13:27:38 +0530

    final submission of 54,55,57,58,59,68,71,72,74,78,87

---
+++ Assignment-57/Assignment 57.1/src/components/ProductList.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './Card'
+
+const ProductList = ({products}) => {
+  return (
+    <div className='flex flex-wrap gap-4'>
+        {products.map(function(items, index){
+            return (
+                <Card
+                    key={index}
+                    img={items.img}
+                    type={items.type}
+                    title={items.title}
+                    price={items.price}
+                    isSale={items.isSale}
+                />
+            );
+        })}
+    </div>
+  )
+}
+
+export default ProductList
